70655c8cdf4bf068511f3fe96d72ce75a728678d
Utkarsh Ashish <utkarshashish@Utkarshs-MacBook-Pro.local>
2021-02-23 14:12:00 +0530

    boiler plate

---
+++ src/test/java/com/example/springtraining2/SpringTraining2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springtraining2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTraining2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
